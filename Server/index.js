fd9a9c38b53aa78348578f18ce91164e6e843a7c
Ranjitha R <r.17ranjitha@gmail.com>
2026-02-20 08:53:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -19,7 +19,6 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function(origin, callback){
-    // allow requests with no origin like mobile apps or curl
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
